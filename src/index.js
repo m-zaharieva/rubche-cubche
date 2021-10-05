6f54b5d8f1f2445fb9ec47aa7aacfe5a9a320ee7
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-10-05 16:13:48 +0300

    load static files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Node Modules
 const express = require('express');
+const path = require('path');
 
 // Local modules
 const handlebars = require('./config/handlebars.js');
@@ -7,6 +8,7 @@ const router = require('./routes.js');
 
 // app
 const app = express();
+app.use(express.static(path.resolve(path.resolve(__dirname, 'static'))));
 handlebars(app);
 app.use(router);
 app.listen(5000, () => console.log('App is running on http://localhost:5000 ...'));
