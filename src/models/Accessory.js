6e110d4e21c0ece214340d136efb9b485965ba44
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-10-10 23:39:10 +0300

    create Accessory model

---
+++ src/models/Accessory.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+let accessorySchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true,
+    }, 
+    imageUrl: {
+        type: String, 
+        required: true,
+        validate: [/https?:\/\//i, 'Invalid image url! The adress should start with http protocol. '],
+    },
+    description: {
+        type: String, 
+        required: true, 
+        maxlength: 500,
+    }
+});
+
+const Accessory = mongoose.model('accessory', accessorySchema);
+
+module.exports = Accessory;
