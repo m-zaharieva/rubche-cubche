926a54962f320cde7dc17efeb25ec3c6c9f2aef2
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-10-10 22:50:43 +0300

    update home controller to asynchronous function

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,14 +1,15 @@
 //Modules
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
 const cubeService = require('./../services/cubeService.js');
 
 
 // Actions
 const home = (req, res) => {
-    let cubes = cubeService.getAll();
-    res.render('index', { cubes });
+    const cubes = cubeService.getAll()
+        .then(cubes => {
+            res.render('index', { cubes });
+        });
 }
 
 const about = (req, res) => {
@@ -23,6 +24,7 @@ const search = (req, res) => {
         cubes
     })
 }
+
 
 // Routers
 router.get('/', home);
