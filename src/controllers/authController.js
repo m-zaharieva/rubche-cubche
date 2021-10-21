2c0e618d89e351ab8ca1ea96c067894aea169f65
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-10-21 10:42:12 +0300

    login user and save a jsonwebtoken as a cookie

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 const authService = require('./../services/authService.js');
+const { TOKEN_COOKIE_NAME } = require('./../constants.js');
+
 
 
 const getRegisterPage = (req, res) => {
@@ -9,18 +11,42 @@ const getRegisterPage = (req, res) => {
 
 const postRegisterPage = async (req, res) => {
     try {
-        let {username, password, repeatPassword } = req.body;
+        let { username, password, repeatPassword } = req.body;
         await authService.registerUser(username, password, repeatPassword);
         res.redirect('/auth/login');
 
     } catch (error) {
-        res.status(400).render('auth/register', {error: error.message})
+        res.status(400).render('auth/register', { error: error.message })
     }
 };
 
 const getLoginPage = (req, res) => {
     res.render('auth/login');
 };
+
+const postLoginPage = (req, res) => {
+    let { username, password } = req.body;
+
+    authService.loginUser(username, password)
+        .then(user => {
+            let token = authService.createToken(user);
+            return Promise.all([user, token]);
+        })
+        .then(([user, token]) => {
+            if (!user) {
+                res.redirect('/404');
+            } else {
+                res.cookie(TOKEN_COOKIE_NAME, token, {
+                    httpOnly: true,
+                });
+                res.redirect('/');
+            }
+        })
+        .catch(err => {
+            res.status(400).redirect('404');
+        })
+};
+
 
 
 router.get('/register', getRegisterPage);
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,18 +1,51 @@
-const User = require('./../models/User.js');
+const bcrypt = require('bcrypt');
 
+const User = require('./../models/User.js');
+const { jwtSign } = require('./../utils/jwtUtils.js');
+const { SECRET } = require('./../constants.js');
 
 
 const registerUser = (username, password, repeatPassword) => {
-    return User.create({ username, password, repeatPassword });
+    return User.create({ username, password });
 };
 
 const loginUser = (username, password) => {
+    
+    return findUser(username)
+        .then(user => {
+            let isValid = bcrypt.compare(password, user.password);
+            return Promise.all([isValid, user]);
+        })
+        .then(([isValid, user]) => {
+            if (isValid) {
+                return user;
+            }
+        })
+        .catch(() => {
+            return null;
+        })
 
+
+}
+
+const createToken = function (user) {
+    let payload = {
+        _id: user._id,
+        username: user.username,
+    };
+
+    return jwtSign(payload, SECRET);
+}
+
+const findUser = (username) => {
+    return User.findOne({ username });
 }
 
 
 const authService = {
     registerUser,
+    loginUser,
+    createToken,
 }
 
 module.exports = authService;
