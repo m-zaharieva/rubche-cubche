2d71c3d07101440e31d245885ba798717ed0aa7c
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-10-23 16:19:54 +0300

    cube model validation

--- src/models/cube.js
+++ src/models/cube.js
@@ -3,17 +3,20 @@ const mongoose = require('mongoose');
 const cubeSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        validate: [/^[a-zA-Z0-9 ]+$/, 'Cube name should have only English letters, digits and whitespaces'],
+        required: true,
+        minlength: [5, 'Cube name should have no less then 5 characters'],
     },
     description: {
         type: String, 
         required: true, 
-        maxlength: 500
+        validate: [/^[a-zA-Z0-9 ]+$/, 'Cube description should have only English letters, digits and whitespaces'],
+        minlength: [20, 'Cube description should have no less then 20 characters' ],
     },
     imageUrl: {
         type: String, 
         required: true,
-        validate: [/https?:\/\//i, 'Invalid image url! The adress should start with http protocol. ']
+        validate: [/^https?:\/\//i, 'Invalid image url! The adress should start with the http protocol. ']
     }, 
     difficulty: {
         type: Number, 
