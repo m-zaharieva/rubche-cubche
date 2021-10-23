aeadc4522150d85fe54afda70a3db7e09f8bfc41
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-10-23 16:18:30 +0300

    create accessory validate input

--- src/models/Accessory.js
+++ src/models/Accessory.js
@@ -4,16 +4,19 @@ let accessorySchema = new mongoose.Schema({
     name: {
         type: String, 
         required: true,
+        minlength: [5, 'Accessory name should have no less then 5 characters'],
+        validate: [/^[a-zA-Z0-9 ]+$/, 'Accessory name should have only English letters, digits and whitespaces.'],
     }, 
     imageUrl: {
         type: String, 
         required: true,
-        validate: [/https?:\/\//i, 'Invalid image url! The adress should start with http protocol. '],
+        validate: [/^https?:\/\//i, 'Invalid image url! The adress should start with http protocol. '],
     },
     description: {
         type: String, 
         required: true, 
-        maxlength: 500,
+        validate: [/^[a-zA-Z0-9 ]+$/, 'Accessory name should have only English letters, digits and whitespaces'],
+        minlength: [20, 'Accessory description should have no less then 20 characters' ],
     }
 });
 
